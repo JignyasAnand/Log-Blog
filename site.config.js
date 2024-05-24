96f4d987876fd4d3fd3ce12450cc565eb7ed910c
Jignyas Anand Siripurapu <93654470+JignyasAnand@users.noreply.github.com>
2024-05-25 00:06:40 +0530

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "TEST123",
+    name: "TEST456",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using node.",
